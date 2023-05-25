4d12cded17008b95a8e9d3b3de2bc84887ff7acd
Harsha <vharsha255@gmail.com>
2023-05-25 10:20:28 +0530

    hasha

---
+++ SnakeAndLadersProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using SnakeLadderProblem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder");
+            int playerposition = 0;
+            Console.WriteLine("singleplayer position is now starting from zero");
+        }
+    }
+}
