b5f6a8ec09018ea8bf1516ff58e41c034aa36c93
Harsha <vharsha255@gmail.com>
2023-05-25 10:32:10 +0530

    hasha

--- SnakeAndLadersProblem/Program.cs
+++ SnakeAndLadersProblem/Program.cs
@@ -21,6 +21,27 @@ namespace SnakeLadderProblem
             playerNumberOfRolls++;
 
             Console.WriteLine("the player rolled a dice is   " + playerDiceRoll);
+            int checkOptions = random.Next(0, 3);
+            switch (checkOptions)
+            {
+                case 0:
+                    Console.WriteLine("No play");
+                    break;
+                case 1:
+                    PlayerPosition += playerDiceRoll;
+                    Console.WriteLine("player position moves a head");
+                    break;
+                case 2:
+                    PlayerPosition -= playerDiceRoll;
+                    Console.WriteLine("player position moves a behind");
+                    break;
+            }
+            if (PlayerPosition < 0)
+            {
+                PlayerPosition = 0;
+            }
+
+            Console.WriteLine("the position of player now  " + PlayerPosition);
         }
     }
 }
